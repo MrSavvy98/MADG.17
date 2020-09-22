43e04271ddc7b6d13a6a7a7938b69e31be627e69
MrSavvy98 <pamuditha.rasanjana99@gmail.com>
2020-09-22 17:12:17 +0530

    login page done and signup page added small changes

--- app/src/main/java/com/example/workfromhome/login.java
+++ app/src/main/java/com/example/workfromhome/login.java
@@ -2,7 +2,10 @@ package com.example.workfromhome;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class login extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button b1 =(Button) findViewById(R.id.btn_signUp);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(login.this, SignUp.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
