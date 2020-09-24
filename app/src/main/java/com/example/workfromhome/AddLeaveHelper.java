9392c2d706a1d00a962665e92dfc29b59a6b02da
MrSavvy98 <pamuditha.rasanjana99@gmail.com>
2020-09-24 17:59:04 +0530

    Leave insert done

---
+++ app/src/main/java/com/example/workfromhome/AddLeaveHelper.java
@@ -0,0 +1,75 @@
+package com.example.workfromhome;
+
+public class AddLeaveHelper {
+
+        String id, depart, leaveType, duration, frm, to, reason;
+
+    public AddLeaveHelper() {
+    }
+
+    public AddLeaveHelper(String id, String depart, String leaveType, String duration, String frm, String to, String reason) {
+        this.id = id;
+        this.depart = depart;
+        this.leaveType = leaveType;
+        this.duration = duration;
+        this.frm = frm;
+        this.to = to;
+        this.reason = reason;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDepart() {
+        return depart;
+    }
+
+    public void setDepart(String depart) {
+        this.depart = depart;
+    }
+
+    public String getLeaveType() {
+        return leaveType;
+    }
+
+    public void setLeaveType(String leaveType) {
+        this.leaveType = leaveType;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getFrm() {
+        return frm;
+    }
+
+    public void setFrm(String frm) {
+        this.frm = frm;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+}
--- app/src/main/java/com/example/workfromhome/ApplyLeave.java
+++ app/src/main/java/com/example/workfromhome/ApplyLeave.java
@@ -3,12 +3,59 @@ package com.example.workfromhome;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ApplyLeave extends AppCompatActivity {
+
+    TextInputLayout emp_id, dept, lType, lDuration, from, to, reason;
+    Button btn_next;
+
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_apply_leave);
+
+        emp_id = findViewById(R.id.emp);
+        dept = findViewById(R.id.dpt);
+        lType = findViewById(R.id.type);
+        lDuration = findViewById(R.id.duration);
+        from = findViewById(R.id.from);
+        to = findViewById(R.id.to);
+        reason = findViewById(R.id.reason);
+        btn_next = findViewById(R.id.next);
+
+        btn_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                rootNode = FirebaseDatabase.getInstance();
+                reference = rootNode.getReference("Leave");
+
+                //get all the values
+
+                String id = emp_id.getEditText().getText().toString();
+                String depart = dept.getEditText().getText().toString();
+                String type = lType.getEditText().getText().toString();
+                String duration = lDuration.getEditText().getText().toString();
+                String frm = from.getEditText().getText().toString();
+                String to1 = to.getEditText().getText().toString();
+                String reason1 = reason.getEditText().getText().toString();
+
+                AddLeaveHelper hel = new AddLeaveHelper(id, depart, type, duration,frm, to1, reason1);
+
+                reference.setValue(hel);
+            }
+        });
+
     }
 }
