8a26d3bd09d14203e356c8030ed7a93b7ba03364
MrSavvy98 <pamuditha.rasanjana99@gmail.com>
2020-09-24 13:15:16 +0530

    Leave pages added

---
+++ app/src/main/java/com/example/workfromhome/Home.java
@@ -0,0 +1,32 @@
+package com.example.workfromhome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+        Button btn1 =(Button) findViewById(R.id.btn_leaveRequest);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Home.this, Leave.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+    }
+}
--- app/src/main/java/com/example/workfromhome/login.java
+++ app/src/main/java/com/example/workfromhome/login.java
@@ -15,12 +15,22 @@ public class login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         Button b1 =(Button) findViewById(R.id.btn_signUp);
+        Button b2 = (Button) findViewById(R.id.btn_login);
 
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(login.this, SignUp.class);
                 startActivity(intent);
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent(login.this, Home.class);
+                startActivity(intent1);
+                finish();
             }
         });
 
