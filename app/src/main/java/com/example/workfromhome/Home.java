07839ca447ece7999784d0f9e4c8b942e54f0168
MrSavvy98 <pamuditha.rasanjana99@gmail.com>
2020-09-27 13:53:10 +0530

    Navigation Drawer added

--- app/src/main/java/com/example/workfromhome/Home.java
+++ app/src/main/java/com/example/workfromhome/Home.java
@@ -1,13 +1,24 @@
 package com.example.workfromhome;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class Home extends AppCompatActivity {
+
+    //variable declaration
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,18 +26,26 @@ public class Home extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
 
-//        Button btn1 =(Button) findViewById(R.id.btn_leaveRequest);
-//
-//        btn1.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//
-//                Intent intent = new Intent(Home.this, Leave.class);
-//                startActivity(intent);
-//            }
-//        });
+        //hooks
 
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+
+        //toolbar
+        setSupportActionBar(toolbar);
+
+        //navigation Drawer menu
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
 
 
     }
+    public void onBackPressed() {
+
+
+    }
+
 }
