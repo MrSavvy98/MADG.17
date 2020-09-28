74e943b6964f0da5c0247845dd8c661930e081f1
MrSavvy98 <pamuditha.rasanjana99@gmail.com>
2020-09-28 17:09:35 +0530

    Navigation Drawer added small changes

--- app/src/main/java/com/example/workfromhome/Home.java
+++ app/src/main/java/com/example/workfromhome/Home.java
@@ -1,18 +1,21 @@
 package com.example.workfromhome;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class Home extends AppCompatActivity {
+public class Home extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     //variable declaration
 
@@ -36,16 +39,34 @@ public class Home extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         //navigation Drawer menu
-
+        navigationView.bringToFront();
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
+        //make the  menu icons  clickable
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+
 
     }
+
+    @Override
     public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
 
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else{
 
+            super.onBackPressed();
+        }
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
 }
