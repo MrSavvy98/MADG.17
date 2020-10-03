e45fcb2d3e93678203fd4e41e469cb4f956f93ee
MrSavvy98 <pamuditha.rasanjana99@gmail.com>
2020-10-03 18:25:35 +0530

    Main activivty name changed

--- app/src/main/java/com/example/workfromhome/Landing.java
+++ app/src/main/java/com/example/workfromhome/Landing.java
@@ -11,12 +11,11 @@ import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import pl.droidsonroids.gif.GifImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class Landing extends AppCompatActivity {
 
     private static int SPLASH_SCREEN = 7000;
 
@@ -30,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_landing);
 
         //Animation
 
@@ -50,14 +49,14 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(MainActivity.this, login.class);
+                Intent intent = new Intent(Landing.this, login.class);
                 Pair[] pairs = new Pair[2];
                 pairs[0] = new Pair<View, String>(image, "logo_image");
                 pairs[1] = new Pair<View, String>(logo, "logo_text");
 
 
                 if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
-                    ActivityOptions option = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this, pairs);
+                    ActivityOptions option = ActivityOptions.makeSceneTransitionAnimation(Landing.this, pairs);
                     startActivity(intent, option.toBundle());
                     finish();
                 }
